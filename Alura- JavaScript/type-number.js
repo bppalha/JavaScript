df17ad161c05307b37572e377993d3c398a45ff5
bppalha <110422159+bppalha@users.noreply.github.com>
2022-08-01 16:07:56 -0300

    Alura-JavaScript

---
+++ Alura- JavaScript/type-number.js
@@ -0,0 +1,24 @@
+//tipo Number
+
+const meuNumero = 3;
+
+const numero1 = 1;
+const numero2 = 2; 
+
+const operacaoMatematica = numero1 - numero2;
+
+console.log(operacaoMatematica)
+
+//ponto flutuante
+
+const pontoFlutuante = 3.3;
+const pontoFlutuanteSemZero= .5; //mesma coisa que 0.5
+
+const novaOperacao = numero1 / pontoFlutuante;
+
+console.log(novaOperacao)
+
+//NaN -> Not a Number(não é um número)
+
+const alura = "alura";
+console.log(alura*numero1)
